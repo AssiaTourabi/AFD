2e2d969e5b44bc9f745cff95247b238b828eed4c
ghita <ghitakhaled14@gmail.com>
2024-03-11 18:44:59 +0000

    hello

--- src/main/java/com/example/automatefini/State.java
+++ src/main/java/com/example/automatefini/State.java
@@ -3,6 +3,7 @@ package com.example.automatefini;
 public class State {
     private int stateNumber;
     private boolean isFinal;
+    private String hello;
 
     public State(int stateNumber, boolean isFinal) {
         this.stateNumber = stateNumber;
